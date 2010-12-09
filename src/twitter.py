8380ffb68846e0defde7414fe9b8bb2eb706efe6
Mikko Ohtamaa <mikko@mfabrik.com>
2010-12-09 15:32:14 +0200

    Päivityksiä

---
+++ src/twitter.py
@@ -0,0 +1,36 @@
+"""
+
+    Generate Twitter link list
+
+"""
+
+input = """@2gen 
+@EsaMatti 
+@Soft 
+@Teemu 
+@akaihola 
+@akhern 
+@ariflinkman 
+@ecyrd 
+@hagnas 
+@jlaurila 
+@kpuputti 
+@linjaaho 
+@mikharj 
+@moo9000 
+@nailor 
+@pyconfinland 
+@roxeteer 
+@saffe 
+@teelmo 
+@tkoola 
+@tsharju 
+@uninen
+"""
+
+print "<ul>"
+for line in input.split("\n"):
+    line = line.strip()
+    line = line.strip("@")
+    print '<li><a href="http://twitter.com/%s">%s</a></li>' % (line, line)
+print "</ul>"    
