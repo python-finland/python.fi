490ffdb7d3d726c751a567c8cd0a22d7a2e73751
Mikko Ohtamaa <mikko@mfabrik.com>
2010-12-09 14:12:50 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+Introduction
+------------
+
+This is web site source code for Python Interest Group Finland.  
+
+All source code is licensed under `BSD license <http://www.opensource.org/licenses/bsd-license.php>`_.
+
+Web site template is from http://freehtml5templates.com
+
+Running development server
+--------------------------
+
+Example::
+
+    ~/google_appengine/dev_appserver.py src
+    
+Contact
+-------
+
+Regarding any questions please contact `Mikko <mikko@mfabrik.com>`_ or 
+PIG-fi mailing list <http://groups.google.com/group/pigfi>`_.
+    
---
+++ src/main.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Main page views.
+
+"""
+
+__author__ = "Mikko Ohtamaa <mikko@mfabrik.com>"
+__copyright__ = "mFabrik Research Oy"
+__license__ = "BSD"
+__docformat__ = "epytext"
+
+# Python standard library imports
+import os
+import random
+import cgi
+import urlparse
+import logging
+
+# Appengine imports
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class PigPage(webapp.RequestHandler):
+    """ Base class for page renderer """
+    
+    def render_page(self, template_file_name):
+        """ Render a page template from templates folder"""
+
+        
+        # Fill in template parameters
+        vars = { }
+       
+        path = os.path.join(os.path.dirname(__file__), 'templates', template_file_name)
+        self.response.out.write(template.render(path, vars))
+
+class MainPage(PigPage):
+    """ Index page of the site.
+
+    """
+    
+    def get(self):
+
+        logging.debug("Loading main page")
+        self.render_page("main.html")
+
+class InEnglishPage(PigPage):
+    """
+    """
+    def get(self):
+        self.render_page("english.html")
+
+class CompaniesPage(PigPage):
+    """
+    """
+    def get(self):
+        self.render_page("companies.html")
+
+
+class NotFound(PigPage):
+    """
+    Handle URIs not found. 
+    """
+    def get(self):
+        self.render_page("404.html")
+
+
+
+application = webapp.WSGIApplication([
+                                ('/english', InEnglishPage),
+                                ('/companies', CompaniesPage),
+                                ('/', MainPage),                                
+                                ('/.*', NotFound),
+                            ], debug=True)
+
+def main():
+    logging.getLogger().setLevel(logging.DEBUG)
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
