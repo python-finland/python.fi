b7b893c418464f4b3efa1e30a3f77eb0e2577c2e
Petri Lehtinen <petri.lehtinen@inoi.fi>
2011-04-11 16:06:49 +0300

    main.py: Fix coding style issues

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-
     Main page views.
-
 """
 
 __author__ = "Mikko Ohtamaa <mikko@mfabrik.com>"
@@ -12,9 +10,6 @@ __docformat__ = "epytext"
 
 # Python standard library imports
 import os
-import random
-import cgi
-import urlparse
 import logging
 
 # Appengine imports
@@ -24,90 +19,79 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 class PigPage(webapp.RequestHandler):
-    """ Base class for page renderer """
-
+    """Base class for page renderer"""
 
     def render_page(self, template_file_name):
         """ Render a page template from templates folder"""
 
-
         # Fill in template parameters
-        vars = { }
+        vars = {}
 
         path = os.path.join(os.path.dirname(__file__), 'templates', template_file_name)
         self.response.out.write(template.render(path, vars))
 
-class MainPage(PigPage):
-    """ Index page of the site.
 
-    """
+class MainPage(PigPage):
+    """Index page of the site"""
 
     def get(self):
 
         logging.debug("Loading main page")
         self.render_page("main.html")
 
+
 class AboutPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("about.html")
 
 
 class InEnglishPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("english.html")
 
+
 class CompaniesPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("companies.html")
 
+
 class BlogsPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("blogs.html")
 
+
 class JobsPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("jobs.html")
 
+
 class LearnPage(PigPage):
-    """
-    """
     def get(self):
         self.render_page("learn.html")
 
 
 class NotFound(PigPage):
-    """
-    Handle URIs not found.
-    """
+    """Handle URIs not found"""
     def get(self):
         self.render_page("404.html")
 
 
-
 application = webapp.WSGIApplication([
-                                ('/about', AboutPage),
-                                ('/english', InEnglishPage),
-                                ('/companies', CompaniesPage),
-                                ('/blogs', BlogsPage),
-                                ('/jobs', JobsPage),
-                                ('/learn', LearnPage),
-                                ('/', MainPage),
-                                ('/.*', NotFound),
-                            ], debug=True)
+    ('/about', AboutPage),
+    ('/english', InEnglishPage),
+    ('/companies', CompaniesPage),
+    ('/blogs', BlogsPage),
+    ('/jobs', JobsPage),
+    ('/learn', LearnPage),
+    ('/', MainPage),
+    ('/.*', NotFound),
+], debug=True)
+
 
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
     run_wsgi_app(application)
+
 
 if __name__ == "__main__":
     main()
