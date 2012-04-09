a91501c668dadd74fbbd497f89601c015ac5ca37
H25 <ndhieu88@gmail.com>
2012-04-09 17:58:05 +0300

    Update script

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ def main():
     output = cmd.stdout.read()
     # check the output and deploy if there is any update
     if output != 'Already up-to-date.\n':
-        subprocess.Popen('~/google_appengine/appcfg.py update src', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        subprocess.Popen('~/google_appengine/appcfg.py --passin --email=pythonfinland@gmail.com update src < gaepass', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 if __name__ == '__main__':
     main()
